008067b3f2e7883a628d4d70ed71443cd868504c
Abhijit Bhadoria <abhijit.bhadoria@M-FVFG91RXQ05N.local>
2025-06-30 19:43:11 +0530

    project structure commit

---
+++ src/main/java/springai/spring_ai_rag_towing_expert/SpringAiRagTowingExpertApplication.java
@@ -0,0 +1,13 @@
+package springai.spring_ai_rag_towing_expert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiRagTowingExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiRagTowingExpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springai/spring_ai_rag_towing_expert/SpringAiRagTowingExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package springai.spring_ai_rag_towing_expert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRagTowingExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
